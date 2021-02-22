4e3af2af9e93e5351c2f2d19b32af21f3ab47d02
prabhashc <chhabra.prabhash@gmail.com>
2021-02-22 17:48:25 +0530

    abc

---
+++ stock/src/com/ofss/TestProduct.java
@@ -0,0 +1,10 @@
+package com.ofss;
+
+public class TestProduct {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		stock st1=new stock(100, "INFY", 123.45);
+		System.out.println(st1);
+	}
+}
